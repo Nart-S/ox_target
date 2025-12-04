170ce81a98418240257e087c6007effc7d476e02
Samuel Nicol <samnicol888@gmail.com>
2025-12-04 14:09:05 +0100

    fix: only set nui focus to false if ox set it to true

--- client/state.lua
+++ client/state.lua
@@ -2,6 +2,7 @@ local state = {}
 
 local isActive = false
 local showVignette = GetConvarInt('ox_target:vignette', 1) == 1
+local ownsFocus = false
 
 ---@return boolean
 function state.isActive()
@@ -20,8 +21,12 @@ function state.setActive(value)
         }))
         SetNuiFocus(true, true)
         SetNuiFocusKeepInput(true)
+        ownsFocus = true
     else
-        SetNuiFocus(false, false)
+        if ownsFocus then
+            SetNuiFocus(false, false)
+            ownsFocus = false
+        end
     end
 end
 
@@ -35,8 +40,15 @@ end
 ---@param value boolean
 function state.setNuiFocus(value, cursor)
     nuiFocus = value
-    SetNuiFocus(value, cursor or false)
-    SetNuiFocusKeepInput(value)
+    if value then
+        SetNuiFocus(value, cursor or false)
+        SetNuiFocusKeepInput(value)
+        ownsFocus = true
+    elseif ownsFocus then
+        SetNuiFocus(false, false)
+        SetNuiFocusKeepInput(false)
+        ownsFocus = false
+    end
 end
 
 local isDisabled = false
